db0ae4bd7ff305a8ecc35e7cbf13447cbc399631
wissal <wissal.el@gmail.com>
2019-05-31 12:59:24 -0700

    Committing all files

---
+++ src/Dogs.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Dogs.css'
+
+class Dogs extends Component{
+    render(){
+        return (
+            <div className='Dogs'>
+               <h1 className='display-1 text-center my-5'>Dog List!</h1>
+                  <div className='row'>
+                     {this.props.dogs.map(d =>(
+                          <div className='Dog col-lg-4 text-center' key={d.name}>
+                              <img src={d.src} alt={d.name}/>
+                              <h3 className='mt-3'>
+                                 <Link className='underline' to={`/dogs/${d.name}`}>{d.name}</Link>
+                              </h3>
+                          </div>   
+                     ))}
+                  </div>
+            </div>
+        )
+    }
+}
+
+export default Dogs;
